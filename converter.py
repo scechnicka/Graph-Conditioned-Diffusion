84d711ace40ba8b0cecb75832bee4ab9acf1b95b
scechnicka <72920082+scechnicka@users.noreply.github.com>
2025-05-19 20:47:38 +0100

    Update converter.py

--- converter.py
+++ converter.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Define the image path
-img_path = "/vol/biomedic3/sc7718/Graph_conditioned_diffusion/graph_prepross_kidney/results1021/results1021_feature_vectors.npy"
+img_path = path/to/resultsX_feature_vectors.npy
 image_data = np.load(img_path)
 # Load the image data from the .npz file
 print(image_data)
